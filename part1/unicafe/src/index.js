bb2bca3656ca05d1121a8b08c5f6e87e19eb9395
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2019-08-26 18:49:23 +0300

    done Ex. 1.6: unicafe step1

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
